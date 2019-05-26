e4030df24468fbd0680538904a11ffcc471eaf15
Ericks <esr@ecomp.poli.br>
2019-05-26 03:41:38 -0300

    Modal Personal Information done for now

--- src/components/PersonalInformation/ModalPersonalInformation/ModalPersonalInformation.test.tsx
+++ src/components/PersonalInformation/ModalPersonalInformation/ModalPersonalInformation.test.tsx
@@ -7,5 +7,28 @@ describe('<PersonalInformaition />', () => {
     beforeEach(() => {
         wrapper = shallow(<ModalPersonalInformation username="user" />);
     });
-    
+    it('has to change state after change inputs', (): void => {
+        wrapper.find("#name").simulate('change', {
+            target: {
+                value: 'name'
+            }
+        });
+        wrapper.find("#job").simulate('change', {
+            target: {
+                value: 'job'
+            }
+        });
+        wrapper.find("#resume").simulate('change', {
+            target: {
+                value: 'resume'
+            }
+        });
+        const modal = wrapper.instance() as ModalPersonalInformation;
+        expect(modal.state).toEqual({
+            name: "name",
+            job: "job",
+            resume: "resume"
+        });
+    });
+
 })
--- src/components/PersonalInformation/ModalPersonalInformation/ModalPersonalInformation.tsx
+++ src/components/PersonalInformation/ModalPersonalInformation/ModalPersonalInformation.tsx
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 interface IProps {
     username: string,
+    name?: string,
+    job?: string,
+    resume?: string
+}
+interface IState {
+    name?: string,
+    job?: string,
+    resume?: string
 }
 
-const modalPersonalInformation: React.FC<IProps> = (props) => (
-    <div>
-        {props.username}
-    </div>
-);
+class ModalPersonalInformation extends Component<IProps, IState>{
 
-export default modalPersonalInformation;
+    componentWillMount() {
+        const { name, job, resume } = this.props;
+        this.setState({
+            name, job, resume
+        });
+    }
+
+    changeNameHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = e.target.value;
+        this.setState({
+            name: value
+        });
+    }
+    changeJobHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = e.target.value;
+        this.setState({
+            job: value
+        });
+    }
+    changeResumeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const value = e.target.value;
+        this.setState({
+            resume: value
+        });
+    }
+
+    public render() {
+        const { name, job, resume } = this.state;
+        return (
+            <Form>
+                <FormGroup>
+                    <Label for="name">Nome:</Label>
+                    <Input type="text" id="name" onChange={this.changeNameHandler} value={name} />
+                    <Label for="job">Profissão:</Label>
+                    <Input type="text" id="job" onChange={this.changeJobHandler} value={job} />
+                    <Label for="resume">Resumo:</Label>
+                    <Input type="textarea" id="resume" onChange={this.changeResumeHandler} value={resume} />
+                </FormGroup>
+            </Form>
+        )
+    }
+
+}
+
+export default ModalPersonalInformation;
